f093d4fd6909745dbd8a86c8e6db973780284bdd
mojibakara <a33306429@gmail.com>
2022-09-20 13:53:55 +0430

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -16,7 +16,7 @@ class Docker implements Serializable {
 
     def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'nexus-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin 185.110.189.54:8085"
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin 185.110.189.54:8084"
         }
     }
 
